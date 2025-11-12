da53162216431ca6e3c38633e3e1370cedc5a1fc
AyannaVN <nobre.ayanna@aluno.ifsp.edu.br>
2025-11-12 20:31:26 -0300

    Subindo alterações finais para teste

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 from utils.db import close_connection
 from routes.usuario_routes import usuario_bp
@@ -6,11 +7,15 @@ from routes.atividade_routes import atividade_bp
 from flask_jwt_extended import JWTManager
 from routes.auth_routes import auth_bp, is_token_revoked
 
+from init_db import init_db
+
 def create_app():
     app = Flask(__name__)
-    import os
     app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', 'chave-padrao-local')
-    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 3600
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = int(os.environ.get('JWT_ACCESS_TOKEN_EXPIRES', 3600))
+    if not os.path.exists('artistick.db'):
+        print("artistick.db não encontrado — criando com init_db()")
+        init_db()
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(usuario_bp)
     app.register_blueprint(filho_bp)
@@ -33,4 +38,4 @@ def create_app():
 import os
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port = os.environ.get('PORT', 3000), debug=True)
+    app.run(host="0.0.0.0", port=init('PORT', 3000), debug=True)
