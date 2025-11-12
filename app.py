62f722f5e78a91ffef9de6296394223186b2ec62
AyannaVN <nobre.ayanna@aluno.ifsp.edu.br>
2025-11-12 20:39:30 -0300

    corrigindo erros

--- app.py
+++ app.py
@@ -7,15 +7,10 @@ from routes.atividade_routes import atividade_bp
 from flask_jwt_extended import JWTManager
 from routes.auth_routes import auth_bp, is_token_revoked
 
-from init_db import init_db
-
 def create_app():
     app = Flask(__name__)
     app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', 'chave-padrao-local')
-    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = int(os.environ.get('JWT_ACCESS_TOKEN_EXPIRES', 3600))
-    if not os.path.exists('artistick.db'):
-        print("artistick.db não encontrado — criando com init_db()")
-        init_db()
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 3600
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(usuario_bp)
     app.register_blueprint(filho_bp)
@@ -38,4 +33,4 @@ def create_app():
 import os
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port=init('PORT', 3000), debug=True)
+    app.run(host="0.0.0.0", port = os.environ.get('PORT', 3000), debug=True)
