12a9fc0d4633d348493f761cca79ea675d615b84
AyannaVN <nobre.ayanna@aluno.isfp.edu.br>
2025-11-13 15:03:11 -0300

    teste

--- utils/protect.py
+++ utils/protect.py
@@ -4,7 +4,7 @@ from flask import jsonify
 def protect_blueprint(bp):
     @bp.before_request
     def _protect():
-        try:
+        #try:
             verify_jwt_in_request()
-        except Exception:
-            return jsonify({"msg": "Token JWT obrigatório ou inválido"}), 401
+        #except Exception:
+        #    return jsonify({"msg": "Token JWT obrigatório ou inválido"}), 401
